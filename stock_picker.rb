ebd90c47dc0d0ce216125a934e034349b5b47466
kiwiofdoom1337 <186161940+kiwiofdoom1337@users.noreply.github.com>
2025-08-29 11:49:10 +0300

    improve logic

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,22 +1,18 @@
 def stock_picker(array)
   profit_days = Array.new()
-  look_for_buy = array[0, array.length - 1]
-  look_for_sell = array[1, array.length]
   profit = 0
   profit_msg = ""
-  scan_count = 0
-  look_for_buy.each do |buy|
-    scan_count += 1
-    look_for_sell.each do |sell|
-      if sell - buy > profit
+
+  array.each_with_index do |buy, buy_day|
+    array.each_with_index do |sell, sell_day|
+      if sell - buy > profit && sell_day > buy_day
         profit = sell - buy
-        profit_days[0] = look_for_buy.index(buy)
-        profit_days[1] = look_for_sell.index(sell) + scan_count
+        profit_days = [buy_day, sell_day]
         profit_msg = "for a profit of $#{sell} - $#{buy} = $#{profit}"
       end
     end
-    look_for_sell.shift()
   end
+
   p profit_days
   puts profit_msg
 end
